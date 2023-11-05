b49ad2bc4469492e5d96c95daae1d048d1523297
zzmrandom <149000037+zzmrandom@users.noreply.github.com>
2023-11-05 15:38:13 +0000

    Js testing

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("hello world");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  console.log(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
