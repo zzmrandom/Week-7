ca494e0c7e3ec1f6964b171351ce7a12660f44ef
zzmrandom <149000037+zzmrandom@users.noreply.github.com>
2023-11-05 16:10:05 +0000

    apikey testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function searchCity(city) {
-  let apiKey = dffcdc392abt6650ob84a0bfdbddfd86;
+  let apiKey = "dffcdc392abt6650ob84a0bfdbddfd86";
   let apiUrl =
     "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
   console.log(apiUrl);
