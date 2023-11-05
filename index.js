f65ccb70c4dedb5c78729ab34067bbd6742b4b09
zzmrandom <149000037+zzmrandom@users.noreply.github.com>
2023-11-05 16:23:41 +0000

    Apikey sorted
    app working - checking data is being received

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+function refreshWeather(response) {
+  console.log(response.data);
+}
+
 function searchCity(city) {
   let apiKey = "dffcdc392abt6650ob84a0bfdbddfd86";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
