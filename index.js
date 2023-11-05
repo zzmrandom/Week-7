8ceead81c2b067a59577e6fa48ec02434613b42d
zzmrandom <149000037+zzmrandom@users.noreply.github.com>
2023-11-05 16:04:44 +0000

    Js apikey testing

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+function searchCity(city) {
+  let apiKey = dffcdc392abt6650ob84a0bfdbddfd86;
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
