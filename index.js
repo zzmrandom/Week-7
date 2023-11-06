ac03ce593bd937dbe6660debd34f7edc0041e0cf
zzmrandom <149000037+zzmrandom@users.noreply.github.com>
2023-11-06 22:08:07 +0000

    update spelling for icon

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function refreshWeather(response) {
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
-  iconElement.innerHTML = `<img src="${repsonse.data.condition.icon_url}" class="weather-app-icon" />`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
