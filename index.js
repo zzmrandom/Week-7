0e97db8568c9fb558b440dc50a457bf1be9b7e28
zzmrandom <149000037+zzmrandom@users.noreply.github.com>
2023-11-06 20:07:05 +0000

    JS
    update on the location and temp showing correctly in round form

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function refreshWeather(response) {
   let tempElement = document.querySelector("#temp");
   let temp = response.data.temperature.current;
-
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
   tempElement.innerHTML = Math.round(temp);
 }
 function searchCity(city) {
@@ -13,10 +14,11 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
