8e682ed3df3c4f9eb9f2144a7fb949aab8d00d31
zzmrandom <149000037+zzmrandom@users.noreply.github.com>
2023-11-06 22:06:58 +0000

    icon html and JS added

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-Speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  iconElement.innerHTML = `<img src="${repsonse.data.condition.icon_url}" class="weather-app-icon" />`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
