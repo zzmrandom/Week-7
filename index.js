7100f3abc18806fd37bcafdf519c0c8372e05166
zzmrandom <149000037+zzmrandom@users.noreply.github.com>
2023-11-06 20:41:59 +0000

    JS wind added

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-Speed");
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   tempElement.innerHTML = Math.round(temp);
 }
 function searchCity(city) {
